1e737b75a4ca24643166165f4b82f5b5f93fded2
unknown <you@example.com>
2018-11-15 07:15:23 +0530

    create some components and adding routes

---
+++ bloggy/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+        mollitia est? Qui quisquam, aspernatur soluta impedit blanditiis
+        ratione, dignissimos vero dicta debitis suscipit recusandae esse quod
+        est velit deserunt inventore.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
